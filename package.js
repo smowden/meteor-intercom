64d639492464d56c16a11a3d5fee14a02b4f3e37
Christian Smorra <smorra.christian@gmail.com>
2014-09-22 17:17:15 +0200

    added version constraints

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-  api.use(['session', 'deps', 'underscore'], 'client');
+  api.use(['session@1.0.0', 'deps@1.0.0', 'underscore@1.0.0'], 'client');
   api.add_files('intercom_server.js', 'server');
   api.add_files(['intercom_loader.js', 'intercom_client.js'], 'client');
   
